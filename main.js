dbff6f32a628d8e0240b5b0a264f7c7b14568715
vortex1603 <amahmudov257@gmail.com>
2024-02-10 17:16:59 +0500

    dzdzddzd

---
+++ main.js
@@ -0,0 +1,15 @@
+const colors = ["red", "green", "blue", "yellow"];
+for (let i = 0; i < colors.length; i++) {
+  console.log(colors[i]);
+}
+let j = 1;
+do {
+  console.log(j * j);
+  j++;
+} while (j <= 10);
+for (let i = 1; i <= 10; i++) {
+  console.log(i * i);
+}
+for (let i = 1; i <= 20; i += 2) {
+  console.log(i);
+}
